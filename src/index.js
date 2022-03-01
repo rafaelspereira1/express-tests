298c7e137d970f8171bd3d4fc0cc03a3f226192a
Rafael Pereira <rafaelspereira1@hotmail.com>
2022-02-28 21:33:11 -0300

    Create new dependency

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (request, response) => response.send("Test Server"));
+
 app.listen(3000, () =>
   console.log("Server is running at http://localhost:3000")
 );
