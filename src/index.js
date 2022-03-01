27c28b2de0aea2e930ff0be98c74ff7078bfde83
Rafael Pereira <rafaelspereira1@hotmail.com>
2022-02-28 22:38:11 -0300

    Add eslint configs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => response.send("Test Server"));
+app.get('/', (request, response) => response.send('Test Server'));
 
-app.listen(3000, () =>
-  console.log("Server is running at http://localhost:3000")
-);
+app.listen(3000, () => console.log('Server is running at http://localhost:3000'));
